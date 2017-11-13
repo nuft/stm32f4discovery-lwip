926b028e3895407a0c4341b81acc8480cd5e2ec4
Michael Spieler <michael.spieler@epfl.ch>
2017-11-13 14:38:53 +0100

    USB serial test

---
+++ main.c
@@ -0,0 +1,58 @@
+#include <ch.h>
+#include <hal.h>
+#include <chprintf.h>
+#include "usbcfg.h"
+
+static THD_WORKING_AREA(waThread1, 128);
+static THD_FUNCTION(Thread1, arg)
+{
+    (void)arg;
+    chRegSetThreadName("blinker");
+    while (true) {
+        palSetPad(GPIOD, GPIOD_LED3);   /* Orange.  */
+        chThdSleepMilliseconds(500);
+        palClearPad(GPIOD, GPIOD_LED3); /* Orange.  */
+        chThdSleepMilliseconds(500);
+    }
+}
+
+
+int main(void)
+{
+    halInit();
+    chSysInit();
+
+    /*
+     * Activates the serial driver 2 using the driver default configuration.
+     * PA2(TX) and PA3(RX) are routed to USART2.
+     */
+    sdStart(&SD2, NULL);
+    palSetPadMode(GPIOA, 2, PAL_MODE_ALTERNATE(7));
+    palSetPadMode(GPIOA, 3, PAL_MODE_ALTERNATE(7));
+
+    /*
+     * Initializes a serial-over-USB CDC driver.
+     */
+    sduObjectInit(&SDU1);
+    sduStart(&SDU1, &serusbcfg);
+
+    /*
+     * Activates the USB driver and then the USB bus pull-up on D+.
+     * Note, a delay is inserted in order to not have to disconnect the cable
+     * after a reset.
+     */
+    usbDisconnectBus(serusbcfg.usbp);
+    chThdSleepMilliseconds(1500);
+    usbStart(serusbcfg.usbp, &usbcfg);
+    usbConnectBus(serusbcfg.usbp);
+
+    /*
+     * Creates the blinker thread.
+     */
+    chThdCreateStatic(waThread1, sizeof(waThread1), NORMALPRIO, Thread1, NULL);
+
+    while (true) {
+        chThdSleepMilliseconds(1000);
+        chprintf((BaseSequentialStream *)&SDU1, "hello world\n");
+    }
+}
